07b5c21dd3eb815cfe006dc3c6b7bfac455e2081
Marcelo Costa <marcel0.c7sta@gmail.com>
2024-12-09 22:28:09 -0300

    salvando as alterações de preço total

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const shoppingKart = () => {
   console.log('Ir para o carrinho');
 };
 
-const decrementIncrement = (id, operation) => {
+const decrementIncrement = (id, operation, idPreco, idTotal) => {
   const qtdPizza = document.getElementById(id);
-
+  const precoPizza = document.getElementById(idPreco);
+  const totalPeperoni = document.getElementById(idTotal);
+  console.log(totalPeperoni)
   if (operation === 'plus') {
     qtdPizza.innerText = parseInt(qtdPizza.innerText) + 1;
-    console.log(qtdPizza);
+    totalPeperoni.innerText = parseInt(qtdPizza.innerText) * parseFloat(precoPizza.innerText); 
+    
     return;
   } else {
     if (parseInt(qtdPizza.innerText) > 0) {
       qtdPizza.innerText = parseInt(qtdPizza.innerText) - 1;
-      console.log(qtdPizza);
+      totalPeperoni.innerText = parseInt(qtdPizza.innerText) * parseFloat(precoPizza.innerText);
       return;
     }
-    console.log(qtdPizza);
+    
   }
 };
+
+
