3eeae0cbf35517763b07aa0605cd5fe267e2bed2
ArlessonMoura <arlessonmss@gmail.com>
2024-12-10 21:06:26 -0300

    ✅ atualização de laurinha

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const shoppingKart = () => {
   console.log('Ir para o carrinho');
 };
 
-const decrementIncrement = (id, operation) => {
+const decrementIncrement = (id, operation, idPreco, idTotal) => {
   const qtdPizza = document.getElementById(id);
-
+  const precoPizza = document.getElementById(idPreco);
+  const precoTotal = document.getElementById(idTotal);
+  console.log(precoTotal)
   if (operation === 'plus') {
     qtdPizza.innerText = parseInt(qtdPizza.innerText) + 1;
-    console.log(qtdPizza);
+    precoTotal.innerText = parseInt(qtdPizza.innerText) * parseFloat(precoPizza.innerText); 
+    
     return;
   } else {
     if (parseInt(qtdPizza.innerText) > 0) {
       qtdPizza.innerText = parseInt(qtdPizza.innerText) - 1;
-      console.log(qtdPizza);
+      precoTotal.innerText = parseInt(qtdPizza.innerText) * parseFloat(precoPizza.innerText);
       return;
     }
-    console.log(qtdPizza);
+    
   }
 };
+
+
